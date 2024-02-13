86813706b59e8c5ea5657a0befee69ba4794578a
Wayne Chen <wchen206@my.bcit.ca>
2024-02-12 17:39:42 -0800

    commented out creature

--- src/Cell.java
+++ src/Cell.java
@@ -1,25 +1,25 @@
 public class Cell {
-  private Creature creature;
+  // private Creature creature;
 
-  public Cell(int index) {
-    creature = new Creature();
-  }
+  // public Cell(int index) {
+  //   creature = new Creature();
+  // }
 
   public boolean callReproduce() {
-
+    return true;
   }
 
   public boolean wantToEat() {
-
+    return true;
   }
 
   public void addCreature() {
 
   }
 
-  public Creature getCreature() {
+  // public Creature getCreature() {
 
-  }
+  // }
 
   public void clear() {
 
