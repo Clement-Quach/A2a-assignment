db22d3abadd518f37fc677ff42c70410d6ee7e88
Wayne Chen <wchen206@my.bcit.ca>
2024-02-12 17:33:39 -0800

    added cell class

---
+++ src/Cell.java
@@ -0,0 +1,34 @@
+public class Cell {
+  private Creature creature;
+
+  public Cell(int index) {
+    creature = new Creature();
+  }
+
+  public boolean callReproduce() {
+
+  }
+
+  public boolean wantToEat() {
+
+  }
+
+  public void addCreature() {
+
+  }
+
+  public Creature getCreature() {
+
+  }
+
+  public void clear() {
+
+  }
+
+  public void die() {
+
+  }
+
+
+
+}
